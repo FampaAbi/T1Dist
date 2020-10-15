dea6ec7b6fc1f2723d6dd7ad22f2b1c1c5ee099e
C-Becker1 <user@DESKTOP-07N8SSF.localdomain>
2020-10-14 22:10:29 -0300

    Added (or at least I hope so) file transfer client-server

--- client.go
+++ client.go
@@ -1,32 +1,47 @@
 package main
 
 import (
-  "log"
+	"log"
+	"os"
+	"time"
 
-  "golang.org/x/net/context"
-  "google.golang.org/grpc"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
 
-  "github.com/Tarea1/Express/logistica"
+	"github.com/Tarea1/Express/logistica"
 )
 
-func main(){
-  var conn *grpc.ClientConn
-  conn, err := grpc.Dial("dist61:9000", grpc.WithInsecure())
-  if err != nil{
-    log.Fatalf("could not connect: %s", err)
-  }
-  defer conn.Close()
+func (c *ClienteGRPC) UploadFile(ctx context.Context, f string) (stats Stats, err error) {
+	file, err = os.Open(f)
+	stream, err := c.client.Upload(ctx)
+	stats.StartedAt = time.Now()
+	buf = make([]byte, c.chunkSize)
+	for writing {
+		n, err = file.Read(buf)
+		stream.Send(&messaging.Chunk{Content: buf[:n]})
+	}
+	stats.FinishedAt = time.Now()
+	status, err = stream.CloseAndRecv()
+}
 
-  c := logistica.NewLogisticaServiceClient(conn)
+func main() {
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial("dist61:9000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect: %s", err)
+	}
+	defer conn.Close()
 
-  message := logistica.Message{
-    Body: "Hello from the client!",
-  }
+	c := logistica.NewLogisticaServiceClient(conn)
 
-  response, err := c.SayHello(context.Background(),&message)
-  if err!= nil{
-    log.Fatalf("Error when calling SayHello: %s", err)
-  }
+	message := logistica.Message{
+		Body: "Hello from the client!",
+	}
 
-  log.Printf("Response from Server: %s", response.Body)
+	response, err := c.SayHello(context.Background(), &message)
+	if err != nil {
+		log.Fatalf("Error when calling SayHello: %s", err)
+	}
+
+	log.Printf("Response from Server: %s", response.Body)
 }
--- server.go
+++ server.go
@@ -1,26 +1,43 @@
 package main
 
-import(
-  "log"
-  "net"
+import (
+	"io"
+	"log"
+	"net"
 
-  "google.golang.org/grpc"
-  "github.com/Tarea1/Express/logistica"
+	"github.com/Tarea1/Express/logistica"
+	"google.golang.org/grpc"
 )
 
-func main(){
-  lis,err := net.Listen("tcp",":9000")
-  if err!= nil {
-    log.Fatalf("Failed to listen on port 9000: %v", err)
-  }
+func (s *ServerGRPC) Upload(stream messaging.GuploadService_UploadServer) (err error) {
+	for {
+		_, err = stream.Recv()
+		if err != nil {
+			if err == io.EOF {
+				goto END
+			}
 
-  s := logistica.Server{}
+			err = error.Wrapf(err, "failed unexpectadely while reading chunks from stream")
+			return
+		}
+	}
+	err = stream.SendAndClose(&messaging.UploadStatus{Message: "Upload received with success", Code: messaging.UploadStatusCode_Ok})
+	return
+}
 
-  grpcServer:= grpc.NewServer()
+func main() {
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("Failed to listen on port 9000: %v", err)
+	}
 
-  logistica.RegisterLogisticaServiceServer(grpcServer, &s)
+	s := logistica.Server{}
 
-  if err := grpcServer.Serve(lis); err!=nil{
-    log.Fatalf("Failed to serve gRPC server over port 9000: %v", err)
-  }
+	grpcServer := grpc.NewServer()
+
+	logistica.RegisterLogisticaServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve gRPC server over port 9000: %v", err)
+	}
 }
